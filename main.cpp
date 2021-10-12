d304581614ea228e389066e0534e28672b709fb8
YenChihLiao <yen.yenchih.liao@gmail.com>
2021-10-12 19:46:56 +0800

    feat: It's moving. sort of

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main(){
 	sf::RenderWindow window(sf::VideoMode(500, 500), "Tyranny");
@@ -7,8 +8,17 @@ int main(){
 
 	while(window.isOpen()){
 		sf::Event event;
+		sf::Vector2i pos = sf::Mouse::getPosition(window);
+		float dx = 0, dy = 0;
 		while(window.pollEvent(event)){
+			// Event window close
 			if(event.type == sf::Event::Closed) window.close();
+			if(event.type == sf::Event::MouseButtonPressed){
+				if(event.key.code == sf::Mouse::Right){
+					std::cout << "clicked" << std::endl;
+					dot.setPosition(pos.x, pos.y);
+				}
+			}
 
 			window.clear();
 			window.draw(dot);
